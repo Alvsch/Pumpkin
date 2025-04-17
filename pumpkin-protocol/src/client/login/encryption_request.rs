aac63d2dd42567a9ec6e77c1afdcc7870ef87273
Alvsch <94403567+Alvsch@users.noreply.github.com>
2025-04-17 18:43:31 +0200

    readable encryption req

--- pumpkin-protocol/src/client/login/encryption_request.rs
+++ pumpkin-protocol/src/client/login/encryption_request.rs
@@ -1,8 +1,10 @@
 use pumpkin_data::packet::clientbound::LOGIN_HELLO;
 use pumpkin_macros::packet;
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 
-#[derive(Serialize, Deserialize)]
+use crate::{ser::NetworkReadExt, ServerPacket};
+
+#[derive(Serialize)]
 #[packet(LOGIN_HELLO)]
 pub struct CEncryptionRequest<'a> {
     pub server_id: &'a str, // 20
@@ -24,5 +26,30 @@ impl<'a> CEncryptionRequest<'a> {
             verify_token,
             should_authenticate,
         }
+    }
+}
+
+#[packet(LOGIN_HELLO)]
+pub struct CEncryptionRequestRead {
+    pub server_id: String, // 20
+    pub public_key: Box<[u8]>,
+    pub verify_token: Box<[u8]>,
+    pub should_authenticate: bool,
+}
+
+impl ServerPacket for CEncryptionRequestRead {
+    fn read(mut read: impl std::io::Read) -> Result<Self, crate::ser::ReadingError> {
+        let server_id = read.get_string_bounded(20)?;
+        let len = read.get_var_int()?.0 as usize;
+        let public_key = read.read_boxed_slice(len)?;
+        let len = read.get_var_int()?.0 as usize;
+        let verify_token = read.read_boxed_slice(len)?;
+
+        Ok(Self {
+            server_id,
+            public_key,
+            verify_token,
+            should_authenticate: read.get_bool()?,
+        })
     }
 }
